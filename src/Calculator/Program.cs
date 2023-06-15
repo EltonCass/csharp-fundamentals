3db6d92bbd5797a02bf11f9c84a90aaa4e01e1cf
Camilo Cortes <cecortesb@gmail.com>
2023-06-14 23:12:21 -0500

    0614 | Command line calculator storing two record history

--- src/Calculator/Program.cs
+++ src/Calculator/Program.cs
@@ -1,109 +1,111 @@
-﻿// Copyright (c) 2023 Elton Cassas. All rights reserved.
-// See LICENSE.txt
+﻿object[,] records = new object[2, 3];
 
-var initialized = false;
-using var cts = new CancellationTokenSource();
-
-Console.CancelKeyPress += (s, e) =>
+for (int i = 0; i < 2; i++)
 {
-    Console.WriteLine();
-    var beforeColor = Console.ForegroundColor;
-    Console.ForegroundColor = ConsoleColor.DarkCyan;
-    Console.WriteLine("Cancelling...");
-    Console.ForegroundColor = beforeColor;
-    Console.WriteLine();
-    cts.Cancel();
+    Console.Write("Enter a mathematical expression: ");
+    string expression = Console.ReadLine();
 
-    if (initialized)
-    {
-        e.Cancel = false;
-    }
-};
+    double result = EvaluateExpression(expression);
+    Console.WriteLine("Result: " + result);
 
-Console.OutputEncoding = System.Text.Encoding.Unicode;
-
-try
-{
-    Console.OutputEncoding = System.Text.Encoding.Unicode;
-
-    initialized = true;
-
-}
-catch (OperationCanceledException)
-{
-    Console.WriteLine();
-    Console.WriteLine("Canceled: Press Any key to exit.");
-    Console.ReadKey();
+    records[i, 0] = result;
+    records[i, 1] = expression;
+    records[i, 2] = DateTime.Now;
 }
 
-//DoOperation("23+34*34");
-//DoOperation("23 +34* 3 /8");
-//DoOperation("23*34+ 3 /8");
-DoOperation("23*34+ 3 /8 + 33-2");
-Console.ReadLine();
+int rowCount = records.GetLength(0);
+int columnCount = records.GetLength(1);
 
-static void DoOperation(string input)
+for (int i = 0; i < rowCount; i++)
 {
-    var multiplicationDivision = new char[] { '*', '/' };
-    var additionSubstraction = new char[] { '+' };
-    var operations = multiplicationDivision.Concat(additionSubstraction).ToArray();
-    input = input.Replace(" ", "");
-    input = input.Replace("-", "+-");
-    // best case
-    var retrievedNumbersString = input.Split(operations, StringSplitOptions.RemoveEmptyEntries);
-
-    // group operations
-    var operationExpressions = new string[retrievedNumbersString.Length - 1];
-    var startingPosition = 0;
-    for (int i = 0; i < retrievedNumbersString.Length - 1; i++)
+    for (int j = 0; j < columnCount; j++)
     {
-        var endPosition = retrievedNumbersString[i].Length + retrievedNumbersString[i + 1].Length + 1;
-        operationExpressions.SetValue(input
-            .Substring(startingPosition, endPosition), i);
-        startingPosition += retrievedNumbersString[i].Length + 1;
+        Console.Write(records[i, j] + "\t");
     }
+    Console.WriteLine();
+}
 
-    var temporalResults = new int[retrievedNumbersString.Length];
-    var index = 0;
-    for (int i = 0; i < operationExpressions.Length; i++)
+static double EvaluateExpression(string expression)
+{
+    double result = 0;
+    double currentNumber = 0;
+    int currentDecimal = 0;
+    char lastOperator = '+';
+    int breakpoint = 0;
+    int operatorCounter = 0;
+    bool isDecimal = false;
+
+    int expressionLength = expression.Length;
+    for (int i = 0; i < expressionLength; i++)
     {
-        var hasMultiplication = operationExpressions[i].Contains("*");
-        if (hasMultiplication || operationExpressions[i].Contains("/"))
+        var c = expression[i];
+        if (char.IsDigit(c))
         {
-            var numbers = operationExpressions[i].Split(multiplicationDivision);
-            var partialResult = hasMultiplication
-                ? int.Parse(numbers[0]) * int.Parse(numbers[1])
-                : int.Parse(numbers[0]) / int.Parse(numbers[1]);
-            if (i != operationExpressions.Length - 1)
+            if (isDecimal)
             {
-                var commonNumber = $"{operationExpressions[i + 1]
-                    .Split($"{retrievedNumbersString[i + 1]}").Last()}";
-                operationExpressions[i + 1] = $"{partialResult}{commonNumber}";
+                currentDecimal = currentDecimal * 10 + (c - '0');
             }
             else
             {
-                temporalResults.SetValue(partialResult, index);
-                index++;
+                currentNumber = currentNumber * 10 + (c - '0');
             }
+        }
+        else if (c == '.')
+        {
+            isDecimal = true;
         }
         else
         {
-            var addingNumbers = operationExpressions[i].Split("+");
-            if (i != operationExpressions.Length - 1)
+            if ((c == '*' || c == '/') && operatorCounter > 0)
             {
-                temporalResults.SetValue(int.Parse(addingNumbers.First()), index);
+                var subExpression = expression.Substring(breakpoint);
+                if (subExpression[0] == '*' || subExpression[0] == '/')
+                {
+                    subExpression = subExpression.Substring(1);
+                }
+                currentNumber = EvaluateExpression(subExpression);
+                currentDecimal = 0;
+                lastOperator = (lastOperator == '-') ? '+' : lastOperator;
+                i = expressionLength;
             }
             else
             {
-                temporalResults.SetValue(int.Parse(addingNumbers.First()), index);
-                index++;
-                temporalResults.SetValue(int.Parse(addingNumbers.Last()), index);
+                ApplyOperator(ref isDecimal, ref result, ref currentNumber, ref currentDecimal, lastOperator);
+                lastOperator = c;
+                operatorCounter = (i != 0) ? operatorCounter + 1 : operatorCounter;
+                breakpoint = i;
             }
-            index++;
         }
     }
 
-    var result = temporalResults.Sum();
+    ApplyOperator(ref isDecimal, ref result, ref currentNumber, ref currentDecimal, lastOperator);
 
-    Console.WriteLine(result);
+    return result;
+}
+
+static void ApplyOperator(ref bool isDecimal, ref double result, ref double currentNumber, ref int currentDecimal, char lastOperator)
+{
+    if (isDecimal)
+    {
+        currentNumber = Convert.ToDouble(currentNumber + "." + currentDecimal);
+        currentDecimal = 0;
+        isDecimal = false;
+    }
+
+    currentNumber = (lastOperator == '-') ? 0 - currentNumber : currentNumber;
+
+    if (lastOperator == '*')
+    {
+        result *= currentNumber;
+    }
+    else if (lastOperator == '/')
+    {
+        result /= currentNumber;
+    }
+    else
+    {
+        result += currentNumber;
+    }
+
+    currentNumber = 0;
 }
