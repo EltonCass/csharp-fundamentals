3ebe07ec6dfb08ef84aa08cb9adaf818e8cfbe75
Camilo Cortes <cecortesb@gmail.com>
2023-06-19 00:17:55 -0500

    0618 | Quiz and User initial classes

---
+++ src/Homework/QuizGame/src/QuizGame.Models/Quiz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Models
+{
+    internal class Quiz
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public HashSet<string> Categories { get; set; }
+        public List<Question> Questions { get; set; }
+
+        public Quiz(string name, string description, HashSet<string> categories, List<Question> questions)
+        {
+            Name = name;
+            Description = description;
+            Categories = categories;
+            Questions = questions;
+        }
+
+        public override string ToString()
+        {
+            string categories = string.Join(", ", Categories);
+            string questions = string.Join("\n  • ", Questions);
+            return $"Quiz Name: {Name}\nDescription: {Description}\nCategories: {categories}\nQuestions:\n  • {questions}";
+        }
+    }
+}
---
+++ src/Homework/QuizGame/src/QuizGame.Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Models
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Score { get; set; }
+        public EmailAddressAttribute Email { get; set; }
+
+        public User(string name, string score, EmailAddressAttribute email,)
+        {
+            Name = name;
+            Score = score;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Username: {Name}\nScore: {Score}\nEmail: {Email}";
+        }
+    }
+}
