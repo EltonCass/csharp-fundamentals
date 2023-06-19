650b998233d89451bf58c4f088dc2a2198acebbf
Camilo Cortes <cecortesb@gmail.com>
2023-06-19 02:00:19 -0500

    0618 | Collection class added

---
+++ src/Homework/QuizGame/src/QuizGame.Models/Collection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Models
+{
+    internal class Collection
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double TotalScore { get; set; }
+        public List<Quiz> Quizzes { get; set; }
+
+        public Collection(string name, string description, string category, List<Quiz> quizzes)
+        {
+            Name = name;
+            Description = description;
+            Category = category;
+            Quizzes = quizzes;
+        }
+    }
+}
